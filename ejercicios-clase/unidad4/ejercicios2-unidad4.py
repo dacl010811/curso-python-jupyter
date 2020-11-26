83cf7a428b1e4a868f6ac2cc2edd63976a78f68b
Darwin Calle <dacl010811@gmail.com>
2020-11-25 20:17:19 -0500

    Ej

--- ejercicios-clase/unidad4/ejercicios2-unidad4.py
+++ ejercicios-clase/unidad4/ejercicios2-unidad4.py
@@ -10,5 +10,23 @@
 
 #Iva por defecto sea = 12%
 
-def facturacion(total, iva):
-    pass
+def facturacion(total, iva=21):
+    """Realiza el calculo de la facturacion
+    Args:
+        total (float): Total de la factura sin impuesto
+        iva (float): IVA. Defaults to 21.
+    Returns:
+        [float]: Total de la factura
+    """
+    return total + total*iva/100 
+
+
+
+if __name__ == "__main__":
+    total_factura = facturacion(1000)
+    print ("El valor total 1  es = ",total_factura)
+
+    total_factura = facturacion(1000,10)  # Invocacion de la funcion modificando el iva  respecto al valor por defecto
+    print ("El valor total 2 es = ",total_factura)
+
+
