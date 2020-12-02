3e2eb2e5c98f9ffc2b47556e8c3fdbc1d2d7cded
Darwin Calle <dacl010811@gmail.com>
2020-12-01 19:29:36 -0500

    OK

--- ejercicios-clase/unidad9/relacion_poo.py
+++ ejercicios-clase/unidad9/relacion_poo.py
@@ -2,14 +2,23 @@
 class Pelicula:
     
     # Constructor de clase
-    def __init__(self, titulo, duracion, lanzamiento):
+    def __init__(self, titulo, duracion, lanzamiento):  # Definicion del constructor
         self.titulo = titulo
         self.duracion = duracion
         self.lanzamiento = lanzamiento
         print('Se ha creado la película:',self.titulo)
-        
+
+    # Metodos  Sobre escritos    
     def __str__(self):
         return '{} ({})'.format(self.titulo, self.lanzamiento)
+    
+    def __del__(self):
+        print("Eliminando")
+    
+    def __len__(self):
+        return self.duracion
+    
+
 
 class Catalogo:
     
@@ -17,7 +26,8 @@ class Catalogo:
     
     def __init__(self,peliculas=[]):  # Denomina Constructor
         self.peliculas = peliculas
-        
+
+    # Metodos    
     def agregar(self,p):  # p será un objeto Pelicula
         self.peliculas.append(p)
         
