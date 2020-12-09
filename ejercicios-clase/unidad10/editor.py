cee176479a6781bd2a5d3c28486da4a1624387df
Darwin Calle <dacl010811@gmail.com>
2020-12-08 22:44:17 -0500

    GUI FINAL

--- ejercicios-clase/unidad10/editor.py
+++ ejercicios-clase/unidad10/editor.py
@@ -2,46 +2,91 @@ from tkinter import *
 from tkinter import filedialog as FileDialog
 from io import open
 
-ruta = ""
+ruta = ""  # La utilizaremos para almacenar la ruta del fichero
 
-#Abrir
-def abrir_archivo():
+
+def nuevo():
     global ruta
-    ruta = FileDialog.askopenfilename(initialdir='.', filetypes=(
-        ("Ficheros de Texto", "*.py")), title="Abrir archivos python")
+    mensaje.set("Nuevo fichero")
+    ruta = ""
+    texto.delete(1.0, "end")
+    root.title("Mi editor")
+
+
+def abrir():
+    global ruta
+    mensaje.set("Abrir fichero")
+    ruta = FileDialog.askopenfilename(
+        initialdir='.',
+        filetypes=(("Ficheros de texto", "*.txt"),),
+        title="Abrir un fichero de texto")
 
     if ruta != "":
         fichero = open(ruta, 'r')
         contenido = fichero.read()
-        text_area.delete(1.0, 'end')
-        text_area.insert('insert', contenido)
+        texto.delete(1.0, 'end')
+        texto.insert('insert', contenido)
         fichero.close()
-        root.title(ruta + " - Mi Editor")
+        root.title(ruta + " - Mi editor")
 
 
-def saludo_evento():
-    print("Hola Mundo desde Tkinter....!")
+def guardar():
+    mensaje.set("Guardar fichero")
+    if ruta != "":
+        contenido = texto.get(1.0, 'end-1c')
+        fichero = open(ruta, 'w+')
+        fichero.write(contenido)
+        fichero.close()
+        mensaje.set("Fichero guardado correctamente")
+    else:
+        guardar_como()
 
 
+def guardar_como():
+    global ruta
+    mensaje.set("Guardar fichero como")
+
+    fichero = FileDialog.asksaveasfile(title="Guardar fichero",
+                                       mode="w", defaultextension=".txt")
+
+    if fichero is not None:
+        ruta = fichero.name
+        contenido = texto.get(1.0, 'end-1c')
+        fichero = open(ruta, 'w+')
+        fichero.write(contenido)
+        fichero.close()
+        mensaje.set("Fichero guardado correctamente")
+    else:
+        mensaje.set("Guardado cancelado")
+        ruta = ""
+
+
+# Configuración de la raíz
 root = Tk()
-root.title("Bloc Notas Python")
-root.iconbitmap('icono.ico')
+root.title("Mi editor")
 
-menubar_principal = Menu(root)
-filemenu = Menu(menubar_principal, tearoff=0)
-filemenu.add_command(label="Nuevo", command="")
-filemenu.add_command(label="Abrir", command=abrir_archivo)
-filemenu.add_command(label="Guardar", command="")
-filemenu.add_command(label="Guardar Como", command="")
+# Menú superior
+menubar = Menu(root)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="Nuevo", command=nuevo)
+filemenu.add_command(label="Abrir", command=abrir)
+filemenu.add_command(label="Guardar", command=guardar)
+filemenu.add_command(label="Guardar como", command=guardar_como)
 filemenu.add_separator()
-filemenu.add_command(label="Salir", command="")
+filemenu.add_command(label="Salir", command=root.quit)
+menubar.add_cascade(menu=filemenu, label="Archivo")
 
-menubar_principal.add_cascade(menu=filemenu, label="Archivo")
+# Caja de texto central
+texto = Text(root)
+texto.pack(fill="both", expand=1)
+texto.config(bd=0, padx=6, pady=4, font=("Consolas", 12))
 
-text_area = Text(root)
-text_area.pack(fill="both", expand=1)
-text_area.config(bd=0, padx=10, pady=10, font=("Verdana", 50))
+# Monitor inferior
+mensaje = StringVar()
+mensaje.set("Bienvenido a tu Editor")
+monitor = Label(root, textvar=mensaje, justify='left')
+monitor.pack(side="left")
 
-
-root.config(menu=menubar_principal)
-root.mainloop()  # Metodo que hace visible la ventana
+root.config(menu=menubar)
+# Finalmente bucle de la apliación
+root.mainloop()
